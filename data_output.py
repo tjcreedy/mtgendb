b2ffb271a636d0a1d27c0d04ae012cd270178393
Luke Swaby-Petts <lukeswabypetts@gmail.com>
2020-07-27 14:22:13 +0100

    Creating replace test files

--- data_output.py
+++ data_output.py
@@ -12,18 +12,30 @@ import gb_csv_module as gcm
 #Create the top-level parser
 parser = argparse.ArgumentParser(description="Extracting data from the database.")
 req_group = parser.add_argument_group('Required arguments')
-req_group.add_argument('--db_user', dest='db_user', metavar='{db_username}', required=True, help="Database username")
-req_group.add_argument('--db_pass', dest='db_pass', metavar='{db_password}', required=True, help="Database password")
-parser.add_argument('-q', '--query', dest='mysql_query', metavar='custom_MySQL_query', help="Custom MySQL query to extract data from database. (e.g. \"SELECT * FROM metadata WHERE country='United Kingdom';\") NOTE: Your custom specification must be enclosed by double quotations, as above.")
+req_group.add_argument('--db_user', dest='db_user', help="Database username", metavar='{db_username}', required=True)
+req_group.add_argument('--db_pass', dest='db_pass', help="Database password", metavar='{db_password}', required=True)
+parser.add_argument('-q', '--query', dest='mysql_query', help="""Custom MySQL 
+query to extract data from database. (e.g. \"SELECT * FROM metadata WHERE 
+country='United Kingdom';\") NOTE: Your custom specification must be enclosed by double quotations, as above.""",
+                    metavar='custom_MySQL_query')
 subparsers = parser.add_subparsers(dest="output_format", description='Desired output format:')
 
 #Create the parser for the 'COUNT' command
-parser_count = subparsers.add_parser('COUNT', help="Prints an integer on the command line —— For help file see 'data_output.py COUNT -h'.")
-parser_count.add_argument('-s', '--specifications', dest='mysql_specs', metavar='{specifications}', nargs='+', help="Comma-separated list of mysql specifications.\n(e.g. 'subregion=Sabah' 'length>25000' 'order=Coleoptera') REMEMBER: Each individual spec must be enclosed by quotations and separated from the next by a space as above.")
+parser_count = subparsers.add_parser('COUNT', help="""Prints an integer on the command line —— 
+For help file see 'data_output.py COUNT -h'.""")
+parser_count.add_argument('-s', '--specifications', dest='mysql_specs', help="""Comma-separated 
+list of mysql specifications.\n(e.g. 'subregion=Sabah' 'length>25000' 'order=Coleoptera') 
+REMEMBER: Each individual spec must be enclosed by quotations and separated from the 
+next by a space as above.""", metavar='{specifications}', nargs='+')
 
 #Create the parser for the 'CSV' command
-parser_csv = subparsers.add_parser('CSV', help="Ouputs a .csv file —— For help file see 'data_output.py CSV -h'.")
-parser_csv.add_argument('-o', '--out', dest = 'output_name', metavar='{Output filename}', required=True, help="Preferred filename for the output (extension will be added automatically according to your output format choice).")
+parser_csv = subparsers.add_parser('CSV', help="""Ouputs a .csv file —— For 
+help file see 'data_output.py CSV -h'.""")
+parser_csv.add_argument('-o', '--out', dest = 'output_name',
+                        help="""Preferred filename for the output (extension 
+                        will be added automatically according to your 
+                        output format choice).""",
+                        metavar='{Output filename}', required=True)
 parser_csv.add_argument('-t', '--table', dest='database_table', metavar='{Table name}', choices=["metadata", "bioentry", "bioentry_dbxref", "bioentry_qualifier_value", "bioentry_reference", "biosequence", "seqfeature", "comment", "taxon", "taxon_name"], help="Name of database table you wish to extract data from. (e.g. metadata.) If provided, the script will assume you require data from every column in the specified table. If this is not the case then stating only the required columns under the -c flag will suffice.")
 parser_csv.add_argument('-c', '--columns', dest='table_columns', metavar='{Column name(s)}', nargs='+', default=['*'], help="Name of table columns you wish to extract data from. (e.g. name length description)")
 parser_csv.add_argument('-s', '--specifications', dest='mysql_specs', metavar='{Specification(s)}', nargs='+', help="Comma-separated list of mysql specifications.\n(e.g. 'subregion=Sabah' 'length>25000' 'order=Coleoptera') NOTE: Each individual specification must be enclosed by quotations and separated from the next by a space as above.")
@@ -42,7 +54,7 @@ parser_gb.add_argument('-g', '--genes', dest='genes', metavar='{gene_names}', na
 
 # args = parser.parse_args(["-sqlu", "root", "-sqlpw", "mmgdatabase", "-db", "mmg_test", "-t", "metadata", "-o", "metadateru", "-s", "subregion='Sabah'"])
 # args = parser.parse_args(["-sqlu", "root", "-sqlpw", "mmgdatabase", "-db", "mmg_test", "-t", "metadata", "-c", "name", "length", "accession", "seq", "-o", "metadateru", "-s", "country='United Kingdon' description='Lucanus sp. BMNH 1425267 mitochondrion, complete genome'", "-f", "csv"])
-# args = parser.parse_args(['--db_user', 'root', '--db_pass', 'mmgdatabase', 'GB', '-o', 'outksis', '-s',  'length>0'])
+# args = parser.parse_args(['--db_user', 'root', '--db_pass', 'mmgdatabase', 'GB', '-o', 'replace', '-s',  'country=China'])
 # args = parser.parse_args(['--db_user', 'root', '--db_pass', 'mmgdatabase', 'FASTA', '-o', 'outksis', '-s',  'country!=Malaysia', '-g' , 'COX2', 'ND3', 'ATP6'])
 # args = parser.parse_args(['--db_user', 'root', '--db_pass', 'mmgdatabase', 'FASTA', '-o', 'outksis', '-s',  'country!=Malaysia', '-g' , '*'])
 # args = parser.parse_args(['--db_user', 'root', '--db_pass', 'mmgdatabase', 'FASTA', '-o', 'malays', '-s',  'species=Stenus boops', '-g', 'COX2', 'ND3', 'ATP6'])
