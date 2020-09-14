93477d268830b37eb784cc9f353d4c1b2f7bffe3
Luke Swaby-Petts <lukeswabypetts@gmail.com>
2020-09-14 10:32:42 +0100

    Testing

--- biosqldb-mysql.mmg.FINAL.sql
+++ biosqldb-mysql.mmg.FINAL.sql
@@ -339,7 +339,7 @@ CREATE TABLE metadata (
 	country	VARCHAR(50),
 	latitude	FLOAT(15),
 	longitude	FLOAT(15),
-	size 	VARCHAR(30),
+	size 	INT(10),
 	feeding_behaviour	VARCHAR(30),
 	habitat		VARCHAR(30),
 	habitat_stratum		VARCHAR(30),
